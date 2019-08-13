7b467c93539c7324e34b6d53c9a9a686cec73ff4
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2019-08-13 21:29:01 +0100

    Move hydras to hydras specific file

--- init-programming.el
+++ init-programming.el
@@ -69,19 +69,7 @@
                   display-buffer-in-side-window)
                  (side            . bottom)
                  (reusable-frames . visible)
-                 (window-height   . 0.15)))
-
-  (defhydra hydra-flycheck
-    (:pre (progn (setq hydra-lv t) (flycheck-list-errors))
-          :post (progn (setq hydra-lv nil) (quit-windows-on "*Flycheck errors*"))
-          :hint nil)
-    "Errors"
-    ("f"  flycheck-error-list-set-filter                            "Filter")
-    ("j"  flycheck-next-error                                       "Next")
-    ("k"  flycheck-previous-error                                   "Previous")
-    ("gg" flycheck-first-error                                      "First")
-    ("G"  (progn (goto-char (point-max)) (flycheck-previous-error)) "Last")
-    ("q"  nil)))
+                 (window-height   . 0.15))))
 
 (use-package lsp-mode
   :commands lsp
