dee8160a1a7fd1a9d79cb297be21a45eeaee0076
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2020-03-14 18:17:18 +0000

    Updated straight bootstrapping

--- init.el
+++ init.el
@@ -28,16 +28,18 @@
 
 
 ;; straight - for checking out packages with git, for local hacking and contributing
-(let ((bootstrap-file (concat user-emacs-directory "straight/bootstrap.el"))
-      (bootstrap-version 2))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+    (load bootstrap-file nil 'nomessage))
 
 
 ;; Install `use-package`
